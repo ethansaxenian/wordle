4d8865b15a09813c9365950ed5d5601f4922f962
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-14 17:02:51 -0500

    finish wordle?

--- src/Grid.js
+++ src/Grid.js
@@ -1,17 +1,28 @@
 import Square from "./Square";
 import { range } from "./utils";
 
-export default function Grid({ attempts, completedRows }) {
-	console.log(completedRows)
+export default function Grid({ attempts, completedRows, word}) {
 	return (
 		<span>
 			{range(0, 6).map((i) => (
 				<span style={{display: 'flex'}} key={`row-${i}`}>
-					{range(0, 5).map((j) => (
-						<span key={`col-${j}`} style={{padding: 5}}>
-							<Square val={attempts[i].word[j]} complete={i < completedRows}/>
-						</span>
-					))}
+					{range(0, 5).map((j) => {
+						const val = attempts[i].word[j];
+						let color = "lightgray";
+						if (val) {
+							if (word.includes(val)) {
+								color = "yellow";
+							}
+							if (word[j] === val) {
+								color = "green";
+							}
+						}
+						return (
+							<span key={`col-${j}`} style={{padding: 5}}>
+								<Square val={i < completedRows ? undefined : val} complete={i < completedRows} color={color}/>
+							</span>
+						)
+					})}
 				</span>
 			))}
 		</span>
