4dc1d57e27f13a78df73baac446cd2d8dcfb5945
Ethan Saxenian <ethansaxenian+github@proton.me>
2022-01-14 17:02:51 -0500

    finish wordle?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Grid from "./Grid";
-import { addLetter, ALPHABET, deleteLetter } from "./utils";
+import { addLetter, ALPHABET, deleteLetter, sample } from "./utils";
 
 export default function App() {
 	const [allWords, setAllWords] = useState();
@@ -13,21 +13,42 @@ export default function App() {
 	const [r4, setR4] = useState([]);
 	const [r5, setR5] = useState([]);
 	const [completedRows, setCompletedRows] = useState(0);
+	const [win, setWin] = useState(false);
 
-	// useEffect(() => {
-	// 	const getWord = async () => {
-	// 		const response = await fetch("https://random-word-api.herokuapp.com/all");
-	// 		if (!response.ok) {
-	// 			throw new Error(response.statusText);
-	// 		}
-	// 		const fetchedWords = await response.json();
-	// 		setAllWords(new Set(fetchedWords));
-	// 		const fiveLetterWords = fetchedWords.filter((word) => word.length === 5);
-	// 		const randomWord = fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
-	// 		setWord(randomWord);
-	// 	}
-	// 	getWord();
-	// }, []);
+	useEffect(() => {
+		const getWord = async () => {
+			const response = await fetch("https://random-word-api.herokuapp.com/all");
+			if (!response.ok) {
+				throw new Error(response.statusText);
+			}
+			const fetchedWords = await response.json();
+			const fiveLetterWords = fetchedWords.filter((word) => word.length === 5);
+			setAllWords(new Set(fiveLetterWords));
+			const randomWord = fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+			setWord(randomWord);
+		}
+		getWord();
+	}, []);
+
+	useEffect(() => {
+		if (completedRows > 0 && completedRows < 6) {
+			setRow(row + 1);
+		}
+		if (word) {
+			[r0, r1, r2, r3, r4, r5].forEach((w) => {
+				if (w.join("") === word) {
+					setWin(true);
+				}
+			});
+		}
+	}, [completedRows]);
+
+	useEffect(() => {
+		setRow(0);
+		setCompletedRows(0);
+		[setR0, setR1, setR2, setR3, setR4, setR5].forEach((fn) => { fn([]) });
+		setWin(false);
+	}, [word]);
 
 	const attempts = {
 		0: { word: r0, fn: setR0 },
@@ -41,20 +62,27 @@ export default function App() {
 	const [currAttempt, setCurrAttempt] = [attempts[row].word, attempts[row].fn];
 
 	const handleKeyDown = (e) => {
-		const key = e.key.toLowerCase();
-		if (ALPHABET.includes(key)) {
-			if (currAttempt.length < 5) {
-				setCurrAttempt(addLetter(currAttempt, key));
-			}
-		} else if (key === "backspace") {
-			if (currAttempt.length > 0) {
-				setCurrAttempt(deleteLetter(currAttempt));
-			}
-		} else if (key === "enter") {
-			if (currAttempt.length === 5) {
-				setCompletedRows(completedRows + 1);
+		if (!win && completedRows < 6) {
+			const key = e.key.toLowerCase();
+			if (ALPHABET.includes(key)) {
+				if (currAttempt.length < 5) {
+					setCurrAttempt(addLetter(currAttempt, key));
+				}
+			} else if (key === "backspace") {
+				if (currAttempt.length > 0) {
+					setCurrAttempt(deleteLetter(currAttempt));
+				}
+			} else if (key === "enter") {
+				if (currAttempt.length === 5) {
+					setCompletedRows(completedRows + 1);
+				}
 			}
 		}
+	}
+
+	const getNewWord = () => {
+		const newWord = sample(Array.from(allWords));
+		setWord(newWord);
 	}
 
   return (
@@ -71,7 +99,19 @@ export default function App() {
 			tabIndex={0}
 		>
 			<h1 style={{paddingBottom: 50, paddingTop: 25}}>WORDLE</h1>
-			<Grid handleKeyDown={handleKeyDown} attempts={attempts} completedRows={completedRows}/>
+			<Grid handleKeyDown={handleKeyDown} attempts={attempts} completedRows={completedRows} word={word}/>
+			{win && (
+				<>
+					<h1>YOU WIN!!!!</h1>
+					<button onClick={getNewWord}>Get new word</button>
+				</>
+			)}
+			{!win && completedRows === 6 && (
+				<>
+					<h1>YOU LOSE</h1>
+					<button onClick={getNewWord}>Get new word</button>
+				</>
+			)}
     </div>
   );
 }
--- src/Square.js
+++ src/Square.js
@@ -1,16 +1,16 @@
-export default function Square({ val, complete }) {
+export default function Square({ val, complete, color }) {
 
 	return (
 		<span
 			style={{
-				border: `2px solid ${val !== undefined ? 'black' : 'lightgray'}`,
+				border: `${complete ? 0 : 2}px solid ${val !== undefined ? 'black' : 'lightgray'}`,
 				width: 62,
 				height: 62,
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
 				fontSize: 40,
-				backgroundColor: complete ? "red" : "white"
+				backgroundColor: complete ? color : "white"
 			}}
 		>
 			{val}
