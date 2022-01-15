b20723b38df28d956ecf5f74ca35b08fb35cf5b8
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-15 11:01:23 -0500

    add toasts

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -4,7 +4,7 @@ export default function Keyboard({ handleKeyDown, history, word }) {
 	const keyboard = [
 		["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
 		["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-		["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"]
+		["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"]
 	]
 
 	return (
