ee16e40f6c1f6df423eaedd791eb509e2a3ccd74
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-18 17:25:48 -0500

    ...

--- src/analytics.js
+++ src/analytics.js
@@ -2,8 +2,7 @@ import ReactGA from "react-ga";
 
 const analytics = () => {
 	ReactGA.initialize(process.env.REACT_APP_GA_ID);
-	ReactGA.send(window.location.pathname + window.location.search);
-	console.log(window.location.pathname + window.location.search);
+	ReactGA.pageview(window.location.pathname + window.location.search);
 };
 
 export default analytics;
