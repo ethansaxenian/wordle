3cce3286617c87e1b452ec14575f81133daab0ed
Ethan Saxenian <ethansaxenian+github@proton.me>
2022-01-18 12:28:38 -0500

    fix ga id

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 const analytics = () => {
-	ReactGA.initialize(process.env.GA_ID);
+	ReactGA.initialize(process.env.REACT_APP_GA_ID);
 	ReactGA.pageview(window.location.pathname + window.location.search);
 };
 
