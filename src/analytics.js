2b8d4c2d04bb8eca6ce9f0e6f820070b9303b388
Ethan Saxenian <esaxenian@middlebury.edu>
2022-01-18 17:12:35 -0500

    env

--- src/analytics.js
+++ src/analytics.js
@@ -2,7 +2,8 @@ import ReactGA from "react-ga";
 
 const analytics = () => {
 	ReactGA.initialize(process.env.REACT_APP_GA_ID);
-	ReactGA.send('https://ethansaxenian.github.io/wordle/');
+	ReactGA.send(process.env.REACT_APP_WEBSITE_URL);
+	console.log(process.env.REACT_APP_GA_ID, process.env.REACT_APP_WEBSITE_URL);
 };
 
 export default analytics;
